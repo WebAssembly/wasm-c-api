WebAssembly/wasm-c-api

commit d71b9dc1db6c8a15fd3f2a001e14ddf7ab912878

Mark source vector as `const` in wasm_<name>_vec_copy (#111)

include/wasm.h
@@ -79,7 +79,7 @@ typedef double float64_t;
     own wasm_##name##_vec_t* out, \
     size_t, own wasm_##name##_t ptr_or_none const[]); \
   void wasm_##name##_vec_copy( \
-    own wasm_##name##_vec_t* out, wasm_##name##_vec_t*); \
+    own wasm_##name##_vec_t* out, const wasm_##name##_vec_t*); \
   void wasm_##name##_vec_delete(own wasm_##name##_vec_t*);
 
 

commit d9a80099d496b5cdba6f3fe8fc77586e0e505ddc

C++ conformance tweaks (#120)
* C++ conformance tweaks

* Fix bug introduced in #111

include/wasm.h
@@ -1,7 +1,7 @@
 // WebAssembly C API
 
-#ifndef __WASM_H
-#define __WASM_H
+#ifndef WASM_H
+#define WASM_H
 
 #include <stddef.h>
 #include <stdint.h>
@@ -701,4 +701,4 @@ static inline void* wasm_val_ptr(const wasm_val_t* val) {
 }  // extern "C"
 #endif
 
-#endif  // #ifdef __WASM_H
+#endif  // #ifdef WASM_H

include/wasm.hh
@@ -1,7 +1,7 @@
 // WebAssembly C++ API
 
-#ifndef __WASM_HH
-#define __WASM_HH
+#ifndef WASM_HH
+#define WASM_HH
 
 #include <cassert>
 #include <cstddef>
@@ -158,7 +158,7 @@ public:
 
   template<class... Ts>
   static auto make(Ts&&... args) -> vec {
-    T data[] = { std::move(args)... };
+    T data[] = { std::forward<Ts>(args)... };
     return make(sizeof...(Ts), data);
   }
 
@@ -743,4 +743,4 @@ public:
 
 }  // namespace wasm
 
-#endif  // #ifdef __WASM_HH
+#endif  // #ifdef WASM_HH

src/wasm-c.cc
@@ -149,7 +149,7 @@ struct borrowed_vec {
   } \
   \
   void wasm_##name##_vec_copy( \
-    wasm_##name##_vec_t* out, wasm_##name##_vec_t* v \
+    wasm_##name##_vec_t* out, const wasm_##name##_vec_t* v \
   ) { \
     wasm_##name##_vec_new(out, v->size, v->data); \
   }
@@ -171,7 +171,7 @@ struct borrowed_vec {
   } \
   \
   void wasm_##name##_vec_copy( \
-    wasm_##name##_vec_t* out, wasm_##name##_vec_t* v \
+    wasm_##name##_vec_t* out, const wasm_##name##_vec_t* v \
   ) { \
     auto v2 = ownvec<Name>::make_uninitialized(v->size); \
     for (size_t i = 0; i < v2.size(); ++i) { \
@@ -632,7 +632,7 @@ void wasm_val_vec_new(
   *out = release_val_vec(std::move(v2));
 }
 
-void wasm_val_vec_copy(wasm_val_vec_t* out, wasm_val_vec_t* v) {
+void wasm_val_vec_copy(wasm_val_vec_t* out, const wasm_val_vec_t* v) {
   auto v2 = vec<Val>::make_uninitialized(v->size);
   for (size_t i = 0; i < v2.size(); ++i) {
     wasm_val_t val;